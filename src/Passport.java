0c3f5879fc7a22f1cabb6cc5979c61325036ea04
damir <damirdikijogurcik@gmail.com>
2025-10-22 08:53:29 +0300

    Initial commit: Passport project with Java console application

---
+++ src/Passport.java
@@ -0,0 +1,17 @@
+public class Passport {
+    public static void main(String[] args) {
+        // Вывод ФИО, группы и информации о себе
+        System.out.println("ФИО: Иванов Иван Иванович");
+        System.out.println("Группа: ПИ-123");
+        System.out.println("О себе:");
+        System.out.println("- Учусь на программиста");
+        System.out.println("- Интересуюсь Java разработкой");
+        System.out.println("- Люблю решать сложные задачи");
+        System.out.println("- В свободное время занимаюсь спортом");
+
+        // Дополнительная информация
+        System.out.println("\nКонтактная информация:");
+        System.out.println("Email: student@university.ru");
+        System.out.println("GitHub: damir");
+    }
+}
